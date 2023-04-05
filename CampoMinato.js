0f03370c0acf14a82dd5de8c18ea418150976636
Leonardo-Cagossi <70197604+Lemmy-21@users.noreply.github.com>
2023-04-05 08:44:49 +0200

    Add files via upload

--- CampoMinato.js
+++ CampoMinato.js
@@ -1,4 +1,28 @@
 const Bombs = 15;
-const height = 20;
-const width = 30;
+const height = 30;
+const width = 20;
+
+window.onload = function generateField () {
+  let strRiga = "<th><th> </th>";
+  for (let i = 0; i < height; i++){
+    strRiga += "<th> </th>";
+  }
+  strRiga += "</tr>";
+  
+
+  t = document.getElementById("Field");
+	t.innerHTML = ""; 
+	t.innerHTML = strRiga;
+
+  for(let i = 0; i < width; i++){
+    strRiga = `<tr><td xId = ${i} yId = 0>X</td>`;
+    for(let j = 0; j < (height-1); j++){strRiga += `<td xId = ${i} yId = ${j+1}>X</td>`}
+    strRiga += "</tr>";
+    t.innerHTML += strRiga;
+  }
+}
+
+
+
+
 
